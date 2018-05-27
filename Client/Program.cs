80fa9d1b1db9e489825f8893c61f63f3d12c79a6
Oleg Tsomkalov <infinitu1327@gmail.com>
2018-05-27 18:53:29 +0300

    Lab2
    RSA

---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Numerics;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            var rnd = new Random();
+            var id = rnd.Next().ToString();
+
+            using (var client = new HttpClient())
+            {
+                var getResponse = await client.GetAsync($"http://localhost:5000/api/RSA/{id}");
+                if (!getResponse.IsSuccessStatusCode) return;
+                var publicKeys =
+                    JsonConvert.DeserializeObject<(BigInteger e, BigInteger n)>(
+                        await getResponse.Content.ReadAsStringAsync());
+
+                Console.WriteLine("Public keys:");
+                Console.WriteLine($"e:{publicKeys.e}");
+                Console.WriteLine($"n:{publicKeys.n}");
+                Console.WriteLine("Enter text for encryption:");
+
+                var text = Console.ReadLine();
+                var encryptedText = RSA.RSA.Encrypt(text, publicKeys);
+
+                Console.WriteLine("Encrypted text (base 64):");
+                Console.WriteLine(encryptedText);
+
+                var form = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("encryptedText", encryptedText)
+                });
+
+                var postResponse = await client.PostAsync($"http://localhost:5000/api/RSA/{id}", form);
+                var decryptedText = await postResponse.Content.ReadAsStringAsync();
+
+                Console.WriteLine("Decrypted text");
+                Console.WriteLine(decryptedText);
+            }
+        }
+    }
+}
---
+++ RSA/RSA.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Numerics;
+using System.Text;
+
+namespace RSA
+{
+    public class RSA
+    {
+        private static readonly Random Rnd = new Random();
+        private readonly (BigInteger d, BigInteger n) _privateKeys;
+        public readonly (BigInteger e, BigInteger n) PublicKeys;
+
+        public RSA()
+        {
+            var p = GetRandomPrime();
+            var q = GetRandomPrime();
+            var n = BigInteger.Multiply(p, q);
+            var f = BigInteger.Multiply(p - 1, q - 1);
+            var e = Rnd.Next(2, 65535);
+
+            while (BigInteger.GreatestCommonDivisor(f, e) != 1) e++;
+
+            BigInteger x = 1;
+            BigInteger d;
+
+            while (true)
+            {
+                d = (x * f + 1) / e;
+
+                if (d * e == x * f + 1) break;
+
+                x++;
+            }
+
+            PublicKeys = (e, n);
+            _privateKeys = (d, n);
+        }
+
+        public static string Encrypt(string text, (BigInteger e, BigInteger n) publicKeys)
+        {
+            var number = new BigInteger(Encoding.UTF8.GetBytes(text));
+
+            BigInteger.ModPow(number, publicKeys.e, publicKeys.n);
+
+            return Convert.ToBase64String(number.ToByteArray());
+        }
+
+        public string Encrypt(string text)
+        {
+            var number = new BigInteger(Encoding.UTF8.GetBytes(text));
+
+            BigInteger.ModPow(number, PublicKeys.e, PublicKeys.n);
+
+            return Convert.ToBase64String(number.ToByteArray());
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            var number = new BigInteger(Convert.FromBase64String(encryptedText));
+
+            BigInteger.ModPow(number, _privateKeys.d, _privateKeys.n);
+
+            return Encoding.UTF8.GetString(number.ToByteArray());
+        }
+
+        private static int GetRandomPrime()
+        {
+            var number = Rnd.Next();
+
+            while (!IsPrime(number)) number++;
+
+            return number;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            for (var i = 2; i < Math.Sqrt(number); i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
